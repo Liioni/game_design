f435acd964eb33aecd4444eae95442ba3fb10f9a
Liioni <40169289+Liioni@users.noreply.github.com>
2023-03-24 13:44:46 +0100

    Added character controlled aim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,20 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float rotationSpeed;
-    private Vector2 move;
+    private Vector2 move, mouseLook, joystickLook;
+    private Vector3 rotationTarget;
+    public bool isPc;
 
     public void OnMove(InputAction.CallbackContext context){
         move = context.ReadValue<Vector2>();
+    }
+    
+    public void OnMouseLook(InputAction.CallbackContext context){
+        mouseLook = context.ReadValue<Vector2>();
+    }
+    
+    public void OnJoystickLook(InputAction.CallbackContext context){
+        joystickLook = context.ReadValue<Vector2>();
     }
 
     private void Start(){
@@ -18,13 +28,54 @@ public class PlayerController : MonoBehaviour
     }
     
     private void Update(){
-        movePlayer();
+        if(isPc){
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(mouseLook);
+
+            if(Physics.Raycast(ray, out hit)){
+                rotationTarget = hit.point;
+            }
+
+            movePlayerWithAim();
+        }else{
+            if(joystickLook.x == 0 && joystickLook.y == 0) {
+                movePlayer();
+            }else{
+                movePlayerWithAim();
+            }
+        }
     }
 
     private void movePlayer(){
         Vector3 movement = new Vector3(move.x, 0f, move.y);
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed);
+        if(movement != Vector3.zero){
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed);
+        }
+
+        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    public void movePlayerWithAim(){
+        if(isPc){
+            var lookPos= rotationTarget - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+
+            Vector3 aimDirection = new Vector3(rotationTarget.x, 0f, rotationTarget.z);
+
+            if(aimDirection != Vector3.zero){
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed);
+            }
+        }else{
+            Vector3 aimDirection = new Vector3(joystickLook.x, 0f, joystickLook.y);
+
+            if(aimDirection != Vector3.zero){
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(aimDirection), rotationSpeed);
+            }
+        }
+
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
 
         transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
     }
