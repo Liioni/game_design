a28ede9cc7983dfdf61c5e0cd2e282f256f9b0cb
Robin <robinnordmark92@gmail.com>
2023-04-26 23:22:41 +0200

    variable distance and angle to player target

--- Assets/Scripts/CaneraFollow.cs
+++ Assets/Scripts/CaneraFollow.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class CaneraFollow : MonoBehaviour
 {
-
+    public GameObject player;
     public Transform target;
+    public Transform mainCamera;
+
     public float smoothTime = 0.3f;
-    public Vector3 offset;
     private Vector3 velocity = Vector3.zero;
+
+    [Range(0f, 100f)]
+    public float d = 50f;
+
+    [Range(0f, 90f)]
+    public float xAngle = 90f;
+
     // Start is called before the first frame update
     void Start(){
+        mainCamera = GameObject.Find("CameraHolder").transform.Find("Main Camera");
+        target = player.transform;
         
+        d = 15f;
+        xAngle = 65f;
     }
 
     // Update is called once per frame
-    void Update(){
+    void LateUpdate(){
         if(target != null){
+            Quaternion targetRotation = Quaternion.Euler(xAngle, 0f, 0f);
+
+            float y = Mathf.Sin(xAngle * Mathf.Deg2Rad) * d;
+            float z = Mathf.Cos(xAngle * Mathf.Deg2Rad) * d;
+
+            Vector3 offset = new Vector3(0, y - target.transform.position.y, -z);
             Vector3 targetPosition = target.position + offset;
 
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+            mainCamera.transform.rotation = Quaternion.Slerp(mainCamera.transform.rotation, targetRotation, smoothTime);
         }
     }
 }
