6135f96b17ef6a9316e3fd43e22ce70d1f4d0045
Robin <robinnordmark92@gmail.com>
2023-04-27 10:54:13 +0200

    change camera view with scrollwheel

--- Assets/Scripts/CaneraFollow.cs
+++ Assets/Scripts/CaneraFollow.cs
@@ -11,11 +11,19 @@ public class CaneraFollow : MonoBehaviour
     public float smoothTime = 0.3f;
     private Vector3 velocity = Vector3.zero;
 
+    private float dMin = 0f;
+    private float dMax = 100f;
+
+    private float xAngleMin = 0f;
+    private float xAngleMax = 90f;
+
     [Range(0f, 100f)]
     public float d = 50f;
 
     [Range(0f, 90f)]
     public float xAngle = 90f;
+
+    private float sensitivity = 10f;
 
     // Start is called before the first frame update
     void Start(){
@@ -29,6 +37,17 @@ public class CaneraFollow : MonoBehaviour
     // Update is called once per frame
     void LateUpdate(){
         if(target != null){
+
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            if (Input.GetKey(KeyCode.LeftControl)) {
+                xAngle -= scroll * sensitivity;
+            } else {
+                d -= scroll * sensitivity;
+            }
+
+            d = Mathf.Clamp(d, dMin, dMax);
+            xAngle = Mathf.Clamp(xAngle, xAngleMin, xAngleMax);
+
             Quaternion targetRotation = Quaternion.Euler(xAngle, 0f, 0f);
 
             float y = Mathf.Sin(xAngle * Mathf.Deg2Rad) * d;
