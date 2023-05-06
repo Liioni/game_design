8b99ae8b425eed33f71501cc08b9c9fb0f5a6866
Robin <robinnordmark92@gmail.com>
2023-05-06 20:11:18 +0200

    variable changes in range enemy

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -26,8 +26,12 @@ public class Enemy : MonoBehaviour
     public void ChaseTarget() {
         if (target == null) { return; }
         Vector3 direction = (target.transform.position - transform.position).normalized;
-        transform.rotation = Quaternion.LookRotation(direction);
         direction.y = 0f;
         gameObject.GetComponent<CharacterController>().Move(direction * movementSpeed * Time.deltaTime);
+    }
+
+    public void LookAtTarget() {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        transform.rotation = Quaternion.LookRotation(direction);
     }
 }
--- Assets/Scripts/Enemies/EnemyType2.cs
+++ Assets/Scripts/Enemies/EnemyType2.cs
@@ -8,38 +8,47 @@ public class EnemyType2 : Enemy
     public Transform bulletSpawnPoint;
     public GameObject bullet;
 
-    public float speed;
-    public float maxDistanceToTarget;
-    public float angleSpread;
-    public int amountOfBullets;
+    private float speed;
+    private float maxDistanceToTarget;
+    private float angleSpread;
+    private int amountOfBullets;
 
-    private float timer = 0.0f;
-    public float bulletInterval;
+    private float timer;
+    private float bulletInterval;
 
     // Start is called before the first frame update
     void Start()
     {
         bulletSpawnPoint = transform.Find("BulletSpawnPoint");
-        speed = 5;
+
+        // Bullet behaviour
+        speed = 4f;
+        amountOfBullets = 3;
+        bulletInterval = 4.0f;
+        angleSpread = 60f;
+
         maxDistanceToTarget = 15f;
-        angleSpread = 90f;
-        amountOfBullets = 10;
-        bulletInterval = 2.0f;
+        timer = bulletInterval;
     }
 
     // Update is called once per frame
     void Update()
     {
-        chaseAndShootTarget();
+        ChaseAndShootTarget();
     }
 
-    void spawnCircleOfBullets(int numberOfBullets)
+    void SpawnCircleOfBullets(int numberOfBullets)
     {
         float angleStep = angleSpread / numberOfBullets;
+        float startAngle = -angleSpread / 2.0f;
+        if (numberOfBullets % 2 == 1)
+        {
+            startAngle += angleStep / 2.0f;
+        }
         Quaternion currentRotation = transform.rotation;
         for (int i = 0; i < numberOfBullets; i++)
         {
-            float angle = (-angleSpread / 2.0f) + (i * angleStep);
+            float angle = startAngle + (i * angleStep);
             GameObject sphere = Instantiate(bullet, transform.position, Quaternion.identity);
             EnemyBullet variables = sphere.GetComponent<EnemyBullet>();
             Vector3 calculatedDirection = new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), 0f, Mathf.Cos(Mathf.Deg2Rad * angle));
@@ -49,7 +58,8 @@ public class EnemyType2 : Enemy
         }
     }
 
-    void chaseAndShootTarget() {
+    void ChaseAndShootTarget() {
+        LookAtTarget();
         timer += Time.deltaTime;
         if (Vector3.Distance(transform.position, target.transform.position) > maxDistanceToTarget)
         {
@@ -59,66 +69,9 @@ public class EnemyType2 : Enemy
         {
             if (timer >= bulletInterval)
             {
-                spawnCircleOfBullets(amountOfBullets);
+                SpawnCircleOfBullets(amountOfBullets);
                 timer = 0f;
             }
         }
     }
 }
-
-//public class Origin : MonoBehaviour
-//{
-    
-//    float timer = 0f;
-//    [SerializeField]
-//    int x;
-//    [SerializeField]
-//    public bool circle;
-//    [SerializeField]
-//    public bool burst;
-//    [SerializeField]
-//    public float interval;
-
-//    System.Random rnd;
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        rnd = new System.Random();
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if (circle)
-//        {
-//            timer += Time.deltaTime;
-//            if (timer > interval)
-//            {
-//                //int randomInt = rnd.Next(5, 11);
-//                spawnCircleOfBullets(x);
-//                timer = 0f;
-//                transform.Rotate(Vector3.up, 10f);
-//            }
-//        }
-
-//        if (burst)
-//        {
-//            timer += Time.deltaTime;
-//            if (timer > interval)
-//            {
-//                for (int i = 0; i < 4; i++)
-//                {
-//                    Vector3 position = transform.position;
-//                    position.x -= i * 0.2f;
-//                    spawnBullet(position, new Vector3(0f, 0f, 1f), 10 / (i * 0.5f + 1));
-//                }
-//                timer = 0f;
-//            }
-//        }
-
-
-//    }
-
-
-//}
