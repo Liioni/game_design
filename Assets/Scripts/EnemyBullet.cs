f888d94eea16e13bd6d26e120b813b5ca209793e
Robin <robinnordmark92@gmail.com>
2023-05-21 14:34:08 +0200

    new material for enemy bullets and fading of bullets via the alpha value

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -5,23 +5,49 @@ using UnityEngine;
 public class EnemyBullet : MonoBehaviour
 {
     public Rigidbody rb;
+    Renderer renderer;
 
     public Vector3 direction;
     public float speed;
+
+    private float timer = 0f;
+    private float destroyTime = 5f;
+
+    private float initialAlpha;
+    private bool fadeOut = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        renderer = GetComponent<Renderer>();
+        initialAlpha = renderer.material.color.a;
+        Destroy(gameObject, destroyTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= destroyTime - 1f) {
+            timer = 0f;
+            fadeOut = true;
+        }
+        if (fadeOut) {
+            fadeBulletOut();
+        }
     }
     private void FixedUpdate()
     {
         transform.Translate(direction * speed * Time.fixedDeltaTime, Space.World);
+    }
+
+    public void fadeBulletOut() {
+        float t = Mathf.Clamp01(timer / 1f);
+        float newAlpha = Mathf.Lerp(initialAlpha, 0f, t);
+        Color newColor = renderer.material.color;
+        newColor.a = newAlpha;
+        renderer.material.color = newColor;
+        Debug.Log(renderer.material.color.a);
     }
 }
